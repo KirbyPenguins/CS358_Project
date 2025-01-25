55cf85ede6ce898af8c6afada5340fd98b5162bf
KirbyPenguins <kfaverty@pdx.edu>
2025-01-25 09:13:05 -0800

    Added the Expressions

--- interp.py
+++ interp.py
@@ -7,14 +7,66 @@ type Operation = rotate | combine
 @dataclass
 class rotate():
     image : Image.Image
+    def __str__(self):
+        return f"rotate({self.image})"
 
 
 @dataclass
 class combine():
     image1 : Image.Image
     image2 : Image.Image
+    def __str__(self):
+        return f"combine({self.image1}, {self.image2})"
 
 type Literal = Name
+type Expr = Add | Sub | Mul | Div | Lit | Let  
+
+
+@dataclass
+class Add():
+    left : Expr
+    right : Expr
+    def __str__(self):
+        return f"Add({self.left}, {self.right})"
+
+
+@dataclass
+class Sub():
+    left : Expr
+    right : Expr
+    def __str__(self):
+        return f"Sub({self.left}, {self.right})"
+
+
+@dataclass
+class Mul():
+    left : Expr
+    right : Expr
+    def __str__(self):
+        return f"Mul({self.left}, {self.right})"
+
+
+@dataclass
+class Div():
+    left : Expr
+    right : Expr
+    def __str__(self):
+        return f"Div({self.left}, {self.right})"
+
+@dataclass
+class Lit():
+    value : int
+    def __str__(self):
+        return f"Literal({self.value})"
+
+
+@dataclass
+class Let():
+    name = str
+    value : Expr
+    body : Expr
+    def __str__(self):
+        return f"(let {self.name} = {self.value} in {self.body})"
 
 
 @dataclass
