d1fd307eef125cca5e9666baaf69d52718cee166
KirbyPenguins <kfaverty@pdx.edu>
2025-01-27 14:41:28 -0800

    Down to 11 Errors

--- interp.py
+++ interp.py
@@ -20,7 +20,61 @@ class combine():
         return f"combine({self.image1}, {self.image2})"
 
 type Literal = Name | Image.Image
-type Expr = Add | Sub | Mul | Div | Lit | Let  
+type Expr = Add | Sub | Mul | Div | Lit | Let  | Neg | And | Or | Not | Eq | Lt | If
+
+
+@dataclass
+class If():
+    condition : Expr
+    then_branch : Expr
+    else_branch : Expr
+    def __str__(self):
+        return f"If({self.condition}, {self.then_branch}, {self.else_branch})"
+
+
+@dataclass
+class Lt():
+    left : Expr
+    right : Expr
+    def __str__(self):
+        return f"lt({self.left}, {self.right})"
+
+
+@dataclass
+class Eq():
+    left : Expr
+    right : Expr
+    def __str__(self):
+        return f"Eq({self.left}, {self.right})"
+
+
+@dataclass
+class Not():
+    value : Expr
+    def __str__(self) -> str:
+        return f"Not({self.value})"
+
+
+@dataclass
+class Or():
+    left : Expr
+    right : Expr
+    def __str__(self) -> str:
+        return f"or({self.left}, {self.right})"
+
+
+@dataclass
+class And():
+    left : Expr
+    right : Expr
+    def __str__(self) -> str:
+        return f"and({self.left}, {self.right})"
+
+@dataclass
+class Neg():
+    value : Expr
+    def __str__(self):
+        return f"Neg({self.value})"
 
 
 @dataclass
@@ -43,7 +97,7 @@ class Sub():
 class Mul():
     left : Expr
     right : Expr
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Mul({self.left}, {self.right})"
 
 
@@ -57,8 +111,8 @@ class Div():
 
 @dataclass
 class Lit():
-    value : int
-    def __str__(self):
+    value : int | bool
+    def __str__(self) -> str:
         return f"Literal({self.value})"
 
 
@@ -117,37 +171,48 @@ def eval(e: Expr) -> Value:
 
 def evalInEnv(env: Env[Value], e: Expr) -> Value:
     match e:
+        case Neg(value):
+            v = evalInEnv(env, value)
+            if type(v) == int:
+                return -v
+            else:
+                raise evalError("Negation requires an integer literal")
+
         # handles the addition
         case Add(left, right):
-            match (evalInEnv(env, left), evalInEnv(env, right)):
-                case (Lit(l), Lit(r)):
-                    return Lit(l + r)
-                case _:
-                    raise evalError("Addition requires two integer literals")
+            left_val = evalInEnv(env, left)
+            right_val = evalInEnv(env, right)
+            if type(left_val) == int and type(right_val) == int:
+                return left_val + right_val
+            else:
+                raise evalError("Addition requires two integers literals")
 
         # handles the subtraction
         case Sub(left, right):
-            match (evalInEnv(env, left), evalInEnv(env, right)):
-                case (Lit(l), Lit(r)):
-                    return Lit(l - r)
-                case _:
-                    raise evalError("Subtraction requires two integer literals")
+            left_val = evalInEnv(env, left)
+            right_val = evalInEnv(env, right)
+            if type(left_val) == int and type(right_val) == int:
+                return left_val - right_val
+            else:
+                raise evalError("Subtraction requires two integer literals")
 
-        #Handles the division
+        #Handles the multiplication
         case Mul(left, right):
-            match (evalInEnv(env, left), evalInEnv(env, right)):
-                case (Lit(l), Lit(r)):
-                    return Lit(l * r)
-                case _:
-                    raise evalError("Multiplication requires two integer literals")
+            left_val = evalInEnv(env, left)
+            right_val = evalInEnv(env, right)
+            if type(left_val) == int and type(right_val) == int:
+                return left_val * right_val
+            else:
+                raise evalError("Multiplication requires two integer literals")
 
         # handles the division
         case Div(left, right):
-            match (evalInEnv(env, left), evalInEnv(env, right)):
-                case (Lit(l), Lit(r)):
-                    return Lit(l / r)
-                case _:
-                    raise evalError("Division requires two integer literals")
+            left_val = evalInEnv(env, left)
+            right_val = evalInEnv(env, right)
+            if type(left_val) == int and type(right_val) == int:
+                return left_val // right_val
+            else:
+                raise evalError("Division requires two integer literals")
 
         # handles the rotate image
         case rotate(image) :
@@ -177,7 +242,81 @@ def evalInEnv(env: Env[Value], e: Expr) -> Value:
         case Let(name, value, body) :
             v = evalInEnv(env, value)
             new_env = extendEnv(env, name, v)
-            return evalInEnv(extendEnv(env, name, v), body)
+            return evalInEnv (new_env, body)
+
+        case Lit(lit):
+            match lit:
+                case int(i):
+                    return i
+                case combine() | rotate():
+                    return Path((lit,))
+                case _:
+                    raise evalError(f"Unknown literal: {lit}")
+
+        case Or(left, right) :
+            left_val = evalInEnv(env, left)
+            right_val = evalInEnv(env, right)
+            if left_val == True:
+                return True
+            elif type(right_val) == int:
+                raise evalError("Or requires two boolean literals")
+            elif type(left_val) == int:
+                raise evalError("Or requires two boolean literals")
+            elif type (right_val) == bool:
+                if right_val == False:
+                    return False
+                else:
+                    return True
+
+
+        case And(left, right) :
+            left_val = evalInEnv(env, left)
+            right_val = evalInEnv(env, right)
+            if right_val == False:
+                return False
+            elif type(right_val) == int:
+                raise evalError("And requires two boolean literals")
+            elif type(left_val) == int:
+                raise evalError("And requires two boolean literals")
+            elif type (right_val) == bool:
+                if right_val == True:
+                    return True
+                else:
+                    return False
+
+
+        case Not(value) :
+            val = evalInEnv(env, value)
+            if val == True:
+                return False
+            elif val == False:
+                return True
+            elif type(val) == int:
+                raise evalError("Not requires a boolean literal")
+
+
+        case Eq(left, right) :
+            l = evalInEnv(env, left)
+            r = evalInEnv(env, right)
+            if l == r:
+                return True
+            else:
+                return False
+
+        case Lt(left, right) :
+            l = evalInEnv(env, left)
+            r = evalInEnv(env, right)
+            if l < r:
+                return True
+            else:
+                return False
+
+        case If(condition, then_branch, else_branch) :
+            c = evalInEnv(env, condition)
+            if c == True:
+                return evalInEnv(env, then_branch)
+            else:
+                return evalInEnv(env, else_branch)
 
         case _:
             raise evalError(f"Unknown expression: {e}")
@@ -206,18 +345,25 @@ image2_path = "Image/image2.jpg"
     
 # Define the expression
 # Define the expression to combine the images
-test_expr = Let(
-    name = "combined_image",  # Name of the variable
-    value = combine(image1_path, image2_path),  # Expression to combine the images
-    body = Name("combined_image")  # Access the resulting combined image
-)
+
+# Define the expression to combine two images twice
 
 # Run the expression
-run(test_expr)
 
+test : Expr = Let('x',
+                   Lit(19),
+                   Let('x',
+                       Lit(20),
+                       Add(Name('x'), Name('x')
+                           )
+                       )
+                   )
+run(test)
 # Here is the link to the Pillow https://pillow.readthedocs.io/en/stable/handbook/tutorial.html
 '''
 The purpose of this project is to create a simple image manipulation language. While 
 using the Pillow library. We will create AST node definitions and then create a run 
-method
+method. The run method will take an expression and evaluate it. The expression will
+be a combination of literals, addition, subtraction, multiplication, division, and
+
 '''
